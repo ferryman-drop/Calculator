4cb497845b356081808af559062329cbc47797c3
Andriy Petelnytskyy <petelnytskyy@gmail.com>
2021-06-09 20:24:31 +0200

    test git 2

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("test gita")
+        print("test gita 2")
 
     }
 }
